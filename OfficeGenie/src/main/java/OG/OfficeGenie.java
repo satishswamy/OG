f894120ca86ea0cc96e5cafd8898f3d5888837b9
satishswamy <satishsswamy@hotmail.com>
2016-02-17 10:34:32 -0600

    new

--- OfficeGenie/src/main/java/OG/OfficeGenie.java
+++ OfficeGenie/src/main/java/OG/OfficeGenie.java
@@ -2,5 +2,6 @@ public class OfficeGenie {
 	public static void main(String Args[]) {
 		System.out.println("Welcome to OfficeGenie System");
 		System.out.println("-- Unique product for unique people --");
+		System.out.println("Programmed by Satish S Swamy");
 	}
 }
